86204bc20a36810d1a2b821ef0ef7b75a2186aaf
DKGKG1979 <161257473+DKGKG1979@users.noreply.github.com>
2026-09-06 23:13:34 +0200

    Update HeadlessSmsSendService.kt

--- app/src/main/java/com/example/smscleaner/HeadlessSmsSendService.kt
+++ app/src/main/java/com/example/smscleaner/HeadlessSmsSendService.kt
@@ -1,11 +1,11 @@
 package com.example.smscleaner
+
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import androidx.appcompat.app.AppCompatActivity
-
-class ComposeSmsActivity : AppCompatActivity()
 
 class HeadlessSmsSendService : Service() {
-    override fun onBind(intent: Intent?): IBinder? = null
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 }
